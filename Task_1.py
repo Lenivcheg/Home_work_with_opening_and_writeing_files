c9d8d3bcbdd56315b0a35da6909c1f9bf43fc3d0
Sergey Nogin <noginn@ya.ru>
2023-05-14 13:50:13 +0300

    first_commit

---
+++ Task_1.py
@@ -0,0 +1,52 @@
+from pprint import pprint
+
+with open('recipe.txt', 'rt', encoding='utf-8') as f:
+    cook_book = {}
+    for line in f:
+        dish_name = line.strip()
+        ingredient_count = int(f.readline())
+        dish = []
+        for _ in range(ingredient_count):
+            core = f.readline()
+            ing, count, unit, = core.strip().split(' | ')
+            dishes = {
+                'ingredient_name': ing,
+                'quantity': count,
+                'measure': unit
+            }
+            dish.append(dishes)
+
+        f.readline()
+        cook_book[dish_name] = dish
+
+
+def get_shop_list_by_dishes(dis, person_count):
+    shop_list = {}
+    if isinstance(dis, list):
+        for b in dis:
+            if b in cook_book:
+                for i in cook_book.get(b):
+                    name, measure, quantity = i.get('ingredient_name'), i.get('measure'), \
+                        int(i.get('quantity')) * person_count
+                    shop_list_1 = {
+                        'measure': measure,
+                        'quantity': quantity
+                    }
+                    shop_list[name] = shop_list_1
+        pprint(shop_list)
+    else:
+        if dis in cook_book:
+            for i in cook_book.get(dis):
+                name, measure, quantity = i.get('ingredient_name'), i.get('measure'), \
+                    int(i.get('quantity')) * person_count
+                shop_list_1 = {
+                    'measure': measure,
+                    'quantity': quantity
+                }
+                shop_list[name] = shop_list_1
+            pprint(shop_list)
+
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 4)
+print('')
+get_shop_list_by_dishes('Омлет', 2)
---
+++ Task_2.py
@@ -0,0 +1,32 @@
+file_list = ['1.txt', '2.txt', '3.txt']
+list_1 = []
+
+
+def open_file(file_name):
+
+    for i in file_name:
+        path = i
+        print(path)
+        with open(path, 'r', encoding='utf-8') as f:
+            a = (f.readlines())
+            test_list = {
+                path: a
+            }
+        list_1.append(test_list)
+
+
+def write_file(file_name):
+    with open('write.txt', 'w', encoding='utf-8') as f:
+        for i in file_name:
+            f.write(str(*i.keys()))
+            f.write('\n')
+            f.write(str(len(*i.values())))
+            f.write('\n')
+            for a in i.values():
+                for b in a:
+                    f.write(b)
+                f.write('\n')
+
+
+open_file(file_list)
+write_file(list_1)
